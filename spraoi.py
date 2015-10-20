d0c502e221b9932c9cba2eafefc7ac83878f76f6
Motasim <MotaSay@gmail.com>
2015-10-20 16:36:30 +0100

    Cleanup and finished the GET request

--- spraoi.py
+++ spraoi.py
@@ -15,8 +15,7 @@ from flask_restful import Resource, Api, reqparse
 from flask_mail import Mail
 
 from spraoimail import send_email_report
-
-import spraoimongo
+from spraoimongo import DB
 
 __author__ = "Alexander O'Connor <Alexander.OConnor@dcu.ie>"
 __credits__ = ["Alexander O'Connor <Alexander.OConnor@dcu.ie>"]
@@ -24,6 +23,9 @@ __version__ = "0.1"
 __email__ = "Alexander O'Connor <Alex.OConnor@dcu.ie>"
 __status__ = "Prototype"
 
+'''
+Main Flask setup
+'''
 app = Flask(__name__)
 app.config.from_object(__name__)
 api  = Api(app)
@@ -41,24 +43,45 @@ MAIL_USERNAME = "Spraoi2015"
 DEFAULT_MAIL_SENDER = "spraoiteam@gmail.com"
 
 '''
+Argument parser. Used to parse the questions retrieval (GET).
+'''
+get_parser = reqparse.RequestParser()
+get_parser.add_argument('username', type=unicode)
+
+'''
 Argument parser. Used to parse the questions submission (PUT).
 '''
-parser = reqparse.RequestParser()
-parser.add_argument('answers', action='append')
+put_parser = reqparse.RequestParser()
+put_parser.add_argument('answers', action='append')
+
+'''
+DB Helper
+'''
+db = DB()
 
 class Questions(Resource):
     '''An API class to handle questions for a specific user.
     GET: retrieves the questions for the provided user.
     PUT: stores the answers for the questions.
     '''
-    def get(self, userid):
+    def get(self):
         '''
-        Returns the remaining questions for the provided user.
+        Returns questions for the provided user.
         '''
         if app.config['TESTING'] == True:
             with codecs.open('data.json', 'r', 'utf-8') as f:
                 questions = json.loads(f.read())
             return questions
+        
+        args = get_parser.parse_args()
+        
+        username = args['username']
+        questions = db.get_questions()
+        
+        return jsonify({
+            'username': username,
+            'questions': questions
+        })
 
     def put(self, userid):
         '''
@@ -78,71 +101,7 @@ class Questions(Resource):
         
         send_email_report(mail, "motasim.alsayed@adaptcentre.ie", correct_answers, chosen_answers)
         
-        
-
-api.add_resource(Questions, '/user/quiz/<string:userid>')
-
-class User(Resource):
-    def get(self, email):
-        if app.config['TESTING'] == True:
-            print "testing"
-            if email == "completeduser@exmaple.com":
-                response = {'completed':'True'}
-            else:
-                response = {'completed' : 'False'}
-        else:
-            if mongoGetUser(email):
-                response = {'completed':'True'}
-            else:
-                response = {'completed':'False'}
-        return response
-
-api.add_resource(User, '/user/<string:email>')
-
-class Quiz(Resource):
-    def get(self):
-        if app.config['TESTING'] == True:
-            #Sorry for the broken formatting
-                questions = [{
-                    "answers": [
-                        {"0":"Not much, really."},
-                        {"1":"Oh, nothing interesting"},
-                        {"2":"Just wanted to say hi."},
-                        {"3":"There's no 4th answer."}
-                    ],
-                    "question_id": 1,
-                    "question_str": "What's up?",
-                    "correct_answer":"0"
-                    },
-                    {
-                    "answers": [
-                        {"0":"Man U."},
-                        {"1":"Barcelona."},
-                        {"2":"Bayern Munich."},
-                        {"3":"Liverpool."}
-                    ],
-                    "question_id": 2,
-                    "question_str": "What's your favorite team?",
-                    "correct_answer":"1"
-                    },
-                {
-                    "answers": [
-                        {"0","Dublin."},
-                        {"1","Galway."},
-                        {"2","Donegal."},
-                        {"3","Belfast."}
-                    ],
-                    "question_id": 2,
-                    "question_str": "What's your favorite city?",
-                    "correct_answer":"1"
-                    }
-                ]
-                response = questions
-        else:
-            response = spraoimongo.mongoGetQuestions()
-        return response
-
-api.add_resource(Quiz, '/questions')
+api.add_resource(Questions, '/questions')
 
 if __name__ == "__main__":
     app.run(debug=True)
